51b0c62b08c96d67b0f84d82b70f1a6ff299f686
Eduard Carreras <ecarreras@gisce.net>
2014-10-08 13:51:15 +0200

    Bump to v0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='flask-admin-openerp',
-    version='0.1.2',
+    version='0.1.3',
     packages=['flask_admin_openerp'],
     url='http://www.gisce.net',
     license='MIT',
