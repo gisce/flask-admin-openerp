78b03a054cfb4bc88673e045957658a9e6c3b505
Eduard Carreras <ecarreras@gisce.net>
2014-10-17 11:34:32 +0200

    Llistem els one2many

--- flask_admin_openerp/form.py
+++ flask_admin_openerp/form.py
@@ -1,9 +1,41 @@
 from flask.ext.admin.form import BaseForm, widgets
 from wtforms import (
     BooleanField, FloatField, StringField,
-    TextAreaField, IntegerField, SelectField
+    TextAreaField, IntegerField, SelectField, SelectMultipleField
 )
-from erppeek import mixedcase
+from wtforms import widgets as wtf_widgets
+from erppeek import mixedcase, Record
+
+
+def coerce_relation(value):
+    if isinstance(value, Record):
+        try:
+            return int(value.id)
+        except:
+            raise TypeError
+    else:
+        return int(value)
+
+
+class ListWidget(wtf_widgets.Select):
+
+    def __call__(self, field, **kwargs):
+        # Render the list
+        kwargs.setdefault('id', 'list_%s' % field.id)
+        html = ['<ul %s>' % wtf_widgets.html_params(**kwargs)]
+        for subfield in field:
+            if subfield.checked:
+                html.append('<li>%s</li>' % subfield.label.text)
+        html.append('</ul>')
+        kwargs.setdefault('id', field.id)
+        kwargs['multiple'] = True
+        html += ['<select %s style="display: none;">' %
+                 wtf_widgets.html_params(name=field.name, **kwargs)]
+        for val, label, selected in field.iter_choices():
+            html.append(self.render_option(val, label, selected))
+        html.append('</select>')
+        return wtf_widgets.HTMLString(''.join(html))
+
 
 
 MAPPING_TYPES = {
@@ -15,7 +47,8 @@ MAPPING_TYPES = {
     'text': TextAreaField,
     'int': IntegerField,
     'selection': SelectField,
-    'many2one': SelectField
+    'many2one': SelectField,
+    'one2many': SelectMultipleField
 }
 
 
@@ -50,13 +83,18 @@ class Form(object):
                 kwargs['widget'] = widgets.DatePickerWidget()
             elif v['type'] == 'datetime':
                 kwargs['widget'] = widgets.DateTimePickerWidget()
-            elif v['type'] == 'many2one':
+            elif v['type'] in ('many2one', 'one2many'):
                 relation = model.client.model(v['relation'])
                 ids = relation.search([])
                 kwargs['choices'] = [
                     (x['id'], x['name']) for x in relation.read(ids, ['name'])
                 ]
-                kwargs['widget'] = widgets.Select2Widget()
-                kwargs['coerce'] = int
+
+                kwargs['coerce'] = coerce_relation
+                if v['type'] == 'many2one':
+                    kwargs['widget'] = widgets.Select2Widget()
+                elif v['type'] == 'one2many':
+                    kwargs['widget'] = ListWidget()
+
             attrs[k] = type_field(label=v['string'], **kwargs)
         return type(class_name, (BaseForm, ), attrs)
